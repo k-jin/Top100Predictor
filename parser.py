57fe9fc7cd5269b3cbea6276d6a0a09a9908ae06
Jaiveer Kothari <jaiveerkothari@yahoo.com>
2015-05-19 02:18:18 -0500

    added code to parser.py

--- parser.py
+++ parser.py
@@ -2,12 +2,17 @@ import json
 from pprint import pprint
 
 
-with open('test.json') as data_file:
+with open('top100.json') as data_file:
 	data = json.load(data_file)
 
-length = len(data['tracks']['items'])
-
+#length = len(data['tracks']['items'])
+length = len(data['items'])
 print length
 
-for songs in range (0, length):
-	pprint(data['tracks']['items'][songs]['name'])
+#for songs in range (0, length):
+#	pprint(data['tracks']['items'][songs]['name'])
+ctr=0
+
+for songs in data['items']:
+	pprint(str(ctr) +" "+ str(songs["track"]["id"])+" "+ str(songs["track"]["popularity"]))
+	ctr+=1
