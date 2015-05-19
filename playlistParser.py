85bad9d9b8bbe78886ad270eb1062f5e10a169b3
kevin <kevinqjin@gmail.com>
2015-05-19 10:56:44 -0500

    cleaned up code

--- playlistParser.py
+++ playlistParser.py
@@ -5,12 +5,9 @@ from pprint import pprint
 with open('top100.json') as data_file:
 	data = json.load(data_file)
 
-#length = len(data['tracks']['items'])
 length = len(data['items'])
 print length
 
-#for songs in range (0, length):
-#	pprint(data['tracks']['items'][songs]['name'])
 ctr=0
 
 for songs in data['items']:
