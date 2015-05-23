9d3d80a124acfb26708d948adad42a65f5e6bee5
jeanettepranin <jeanettepranin@gmail.com>
2015-05-22 21:52:44 -0500

    Change searchParserNoParams.py to accomodate for searchResultParser.py being in the directory a-z_music

--- searchParserNoParams.py
+++ searchParserNoParams.py
@@ -13,28 +13,29 @@ def make_music_csv():
 	file_list = file_list[1:]
 
 	for music_file in file_list:
-		#loads each json file
-		with open("a-z_music/" + music_file) as data_file:
-			data = json.load(data_file)
-		
-		dataItems  = data['tracks']['items']
-		length = len(dataItems)
+		if music_file[-5:] == ".json":
+			#loads each json file
+			with open("a-z_music/" + music_file) as data_file:
+				data = json.load(data_file)
+			
+			dataItems  = data['tracks']['items']
+			length = len(dataItems)
 
-		tracks = {}
+			tracks = {}
 
-		for songs in range (0, length):
-			currentTrack = dataItems[songs]
-			tracks[currentTrack['name'].encode('utf-8')] = {'song_name':currentTrack['name'].encode('utf-8'),'album':currentTrack['album']['name'].encode('utf-8'), 'available_markets':currentTrack['available_markets'], 'duration_ms':currentTrack['duration_ms'], 'explicit':currentTrack['explicit'], 'popularity':currentTrack['popularity']}
-			tracks[currentTrack['name'].encode('utf-8')]['artists'] = []
-			artistLength = len(currentTrack['artists'])
-			for artists in range (0, artistLength):
-				tracks[currentTrack['name'].encode('utf-8')]['artists'].append(currentTrack['artists'][artists]['name'].encode('utf-8'))
-			#print tracks[currentTrack['name'].encode('utf-8')]['available_markets']
-			for i in range (0,len(tracks[currentTrack['name'].encode('utf-8')]['available_markets'])):
-				tracks[currentTrack['name'].encode('utf-8')]['available_markets'][i] = tracks[currentTrack['name'].encode('utf-8')]['available_markets'][i].encode('utf-8')
+			for songs in range (0, length):
+				currentTrack = dataItems[songs]
+				tracks[currentTrack['name'].encode('utf-8')] = {'song_name':currentTrack['name'].encode('utf-8'),'album':currentTrack['album']['name'].encode('utf-8'), 'available_markets':currentTrack['available_markets'], 'duration_ms':currentTrack['duration_ms'], 'explicit':currentTrack['explicit'], 'popularity':currentTrack['popularity']}
+				tracks[currentTrack['name'].encode('utf-8')]['artists'] = []
+				artistLength = len(currentTrack['artists'])
+				for artists in range (0, artistLength):
+					tracks[currentTrack['name'].encode('utf-8')]['artists'].append(currentTrack['artists'][artists]['name'].encode('utf-8'))
+				#print tracks[currentTrack['name'].encode('utf-8')]['available_markets']
+				for i in range (0,len(tracks[currentTrack['name'].encode('utf-8')]['available_markets'])):
+					tracks[currentTrack['name'].encode('utf-8')]['available_markets'][i] = tracks[currentTrack['name'].encode('utf-8')]['available_markets'][i].encode('utf-8')
 
-		music_dicts[music_file] = tracks
-		i+=1
+			music_dicts[music_file] = tracks
+			i+=1
 
 
 	with open('test_songs.csv', 'ab') as csvfile:
