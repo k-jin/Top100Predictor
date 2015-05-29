6afafbd92f8e0c9b09cbc41bad036299aca759db
k-jin <kevinqjin@gmail.com>
2015-05-29 16:56:07 -0500

    renamed parser 2

--- a-z_music/parserPlusArtist.py
+++ a-z_music/parserPlusArtist.py
@@ -13,21 +13,18 @@ with open(sys.argv[1]) as data_file:
 	data = json.load(data_file)
 
 dataItems  = data['tracks']['items']
-length = len(dataItems)
-
 tracks = {}
 
 
-for i in range(0,length):
+for i in range(0,len(dataItems)):
 
 	currentTrack = dataItems[i]
-	tracks[currentTrack['name'].encode('utf-8')] = {'song_name':currentTrack['name'].encode('utf-8'),'album':currentTrack['album']['name'].encode('utf-8'), 'available_markets':currentTrack['available_markets'], 'duration_ms':currentTrack['duration_ms'], 'explicit':currentTrack['explicit'], 'popularity':currentTrack['popularity']}
+	tracks[currentTrack['name'].encode('utf-8')] = {'song_name':currentTrack['name'].encode('utf-8'),'album':currentTrack['album']['name'].encode('utf-8'), 'duration_ms':currentTrack['duration_ms'], 'explicit':currentTrack['explicit'], 'song_id': currentTrack['id']}
 
 	
 	tracks[currentTrack['name'].encode('utf-8')]['artists'] = []
 
 	artistLength = len(currentTrack['artists'])
-	
 	
 	tracks[currentTrack['name'].encode('utf-8')]['artist_id']=currentTrack['artists'][0]['id'].encode('utf-8')
 
@@ -46,15 +43,15 @@ for i in range(0,length):
 		tracks[currentTrack['name'].encode('utf-8')]['available_markets'][i] = tracks[currentTrack['name'].encode('utf-8')]['available_markets'][i].encode('utf-8')
 # pprint(tracks)
 
-
+print tracks['A Sky Full of Stars']
 	#print tracks[currentTrack['name'].encode('utf-8')]
 
-csv_name = 'songs_' + str(sys.argv[1][0]) + '.csv'
-with open(csv_name, 'ab') as csvfile:
-	fieldnames = ['song_name', 'artists', 'album', 'duration_ms', 'popularity', 'explicit', 'available_markets','artist_id','artist_popularity','num_of_artists', 'num_artist_followers']
-	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-	for key in tracks:
-		writer.writerow(tracks[key])
+# csv_name = 'songs_' + str(sys.argv[1][0]) + '.csv'
+# with open(csv_name, 'ab') as csvfile:
+# 	fieldnames = ['song_name', 'song_id', 'artists', 'album', 'duration_ms', 'explicit', 'artist_id','artist_popularity','num_of_artists', 'num_artist_followers']
+# 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# 	for key in tracks:
+# 		writer.writerow(tracks[key])
 
 
 	#url = urlopen('https://api.spotify.com/v1/tracks/{id}').read()
