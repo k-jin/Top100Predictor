91a8ac1ae7447a71fb16775bcf2e14324fe7380e
jeanettepranin <jeanettepranin@gmail.com>
2015-06-01 12:14:10 -0500

    parserPlusArtist works, just need to fix API rate limit issue

--- a-z_music/parserPlusArtist.py
+++ a-z_music/parserPlusArtist.py
@@ -14,41 +14,64 @@ with open(sys.argv[1]) as data_file:
 
 dataItems  = data['tracks']['items']
 tracks = {}
+ens1="http://developer.echonest.com/api/v4/song/profile?api_key=JPGIYQEE2JFVVQFIZ&track_id=spotify:track:"
 
+ens2="&bucket=id:spotify&bucket=audio_summary"
 
-for i in range(0,len(dataItems)):
+print "len of dataItems "+str(len(dataItems))
 
+#for i in range(0,len(dataItems)):
+for i in range(0,10):
 	currentTrack = dataItems[i]
-	tracks[currentTrack['name'].encode('utf-8')] = {'song_name':currentTrack['name'].encode('utf-8'),'album':currentTrack['album']['name'].encode('utf-8'), 'duration_ms':currentTrack['duration_ms'], 'explicit':currentTrack['explicit'], 'song_id': currentTrack['id'].encode['utf-8']}
+	curTrack=currentTrack['name'].encode('utf-8')
+
+	tracks[curTrack] = {'song_name':currentTrack['name'].encode('utf-8'),'album':currentTrack['album']['name'].encode('utf-8'), 'duration_ms':currentTrack['duration_ms'], 'explicit':currentTrack['explicit'], 'song_id': currentTrack['id'].encode('utf-8')}
 	
-	tracks[currentTrack['name'].encode('utf-8')]['artists'] = []
+	tracks[curTrack]['artists'] = []
 
 	artistLength = len(currentTrack['artists'])
 	
-	tracks[currentTrack['name'].encode('utf-8')]['artist_id']=currentTrack['artists'][0]['id'].encode('utf-8')
+	tracks[curTrack]['artist_id']=currentTrack['artists'][0]['id'].encode('utf-8')
 
-	artist_info=json.loads(urlopen("https://api.spotify.com/v1/artists/"+str(tracks[currentTrack['name'].encode('utf-8')]['artist_id'])).read())
+	artist_info=json.loads(urlopen("https://api.spotify.com/v1/artists/"+str(tracks[curTrack]['artist_id'])).read())
 	
-	tracks[currentTrack['name'].encode('utf-8')]['artist_popularity']=artist_info['popularity']
-	tracks[currentTrack['name'].encode('utf-8')]['num_of_artists']=artistLength 
-	tracks[currentTrack['name'].encode('utf-8')]['num_artist_followers']=artist_info["followers"]["total"]
+	tracks[curTrack]['artist_popularity']=artist_info['popularity']
+	tracks[curTrack]['num_of_artists']=artistLength 
+	tracks[curTrack]['num_artist_followers']=artist_info["followers"]["total"]
 
+	enartist_url=ens1+str(currentTrack['id'].encode('utf-8') )+ens2
 
+	response_info=json.loads(urlopen(enartist_url).read())
+	#print response_info
+	audioShortcut = response_info['response']['songs'][0]['audio_summary']
+	#print audioShortcut
+
+	#audioDict['song_name'] = audioShortcut[5]
+	#audioDict['artist_name'] = audioShortcut[2]
+	tracks[curTrack]['danceability'] = audioShortcut['danceability']
+	tracks[curTrack]['energy'] =  audioShortcut['energy']
+	tracks[curTrack]['loudness'] = audioShortcut['loudness']
+	tracks[curTrack]['speechiness'] = audioShortcut['speechiness']
+	tracks[curTrack]['tempo'] = audioShortcut['tempo']
+	tracks[curTrack]['song_title_en'] = response_info['response']['songs'][0]['title'].encode('utf-8')
 
 	for artists in range (0, artistLength):
 		tracks[currentTrack['name'].encode('utf-8')]['artists'].append(currentTrack['artists'][artists]['name'].encode('utf-8'))
 
-# pprint(tracks)
 
-print tracks['A Sky Full of Stars']
 	#print tracks[currentTrack['name'].encode('utf-8')]
 
-# csv_name = 'songs_' + str(sys.argv[1][0]) + '.csv'
-# with open(csv_name, 'ab') as csvfile:
-# 	fieldnames = ['song_name', 'song_id', 'artists', 'album', 'duration_ms', 'explicit', 'artist_id','artist_popularity','num_of_artists', 'num_artist_followers']
-# 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-# 	for key in tracks:
-# 		writer.writerow(tracks[key])
+# pprint(tracks)
+
+#print tracks['A Sky Full of Stars']
+	#print tracks[currentTrack['name'].encode('utf-8')]
+
+csv_name = 'songs_' + str(sys.argv[1][0]) + '.csv'
+with open(csv_name, 'ab') as csvfile:
+	fieldnames = ['song_name', 'song_id', 'artists', 'album', 'duration_ms', 'explicit', 'artist_id','artist_popularity','num_of_artists', 'num_artist_followers','danceability','energy','loudness','speechiness','tempo','song_title_en']
+ 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+ 	for key in tracks:
+ 		writer.writerow(tracks[key])
 
 
 	#url = urlopen('https://api.spotify.com/v1/tracks/{id}').read()
@@ -56,4 +79,32 @@ print tracks['A Sky Full of Stars']
 #JPGIYQEE2JFVVQFIZ
 
 # http://developer.echonest.com/api/v4/song/profile?api_key=JPGIYQEE2JFVVQFIZ&track_id=spotify:track:4toSP60xmDNCFuXly8ywNZ&bucket=id:spotify&bucket=audio_summary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
